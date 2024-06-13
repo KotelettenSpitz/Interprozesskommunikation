af4ce341463a29d9d68d6684ba9ef4f548a0f8d0
Manuel <manuel@kuenkel.org>
2024-06-13 02:17:30 +0200

    + Main.py
    Main Methode zum Ausführen der einzelnen Dateien wurde hinzugefügt.

---
+++ Shared Memory/main.py
@@ -0,0 +1,23 @@
+import time, subprocess, os, sys
+
+Dateien = ["Conv.py", "Log.py", "Stat.py", "Report.py"]
+
+folder_path = os.path.dirname(__file__)
+
+def run_file(file_name):
+    Farbe = ["\033[92m"]
+    Standardfarbe = "\033[0m"
+    print(f"{Farbe[os.getpid() % len(Farbe)]}Ausführen von {file_name}...{Standardfarbe}", flush=True)
+    subprocess.run(["/usr/bin/python3", file_name], stdout=sys.stdout)
+    print(f"{Farbe[os.getpid() % len(Farbe)]}{file_name} wurde ausgeführt.{Standardfarbe}", flush=True)
+
+for Datei_Name in Dateien:
+    pid = os.fork()
+    if pid == 0:
+        run_file(Datei_Name)
+        exit()
+    time.sleep(0.75)
+
+for _ in range(len(Dateien)):
+    os.wait()
+
