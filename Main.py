cd690f87335f2a24dd956ff5dace0581bcba4e42
OrhanYu <orhan.akcora@outlook.de>
2024-06-13 21:42:48 +0200

    Code schlanker gestaltet

--- Main.py
+++ Main.py
@@ -49,108 +49,71 @@ if __name__ == "__main__":
 
     input = input("Wählen sie ihre Implementierungsvariante: ")
 
-#------------------------------------------------------------------------------------------------------    
+#------------------------------------------------------------------------------------------------------  
+
+    if input == "M":
+        MessageQueues.CreateQueue()
+    
+    if input == "P":
+        PipesKelvin.benannte_pipes()
+
+    ProzessIDs = []
+    
+    ProzessIDs.append(os.fork())
+    if ProzessIDs[-1] == 0:
+        if input == "T":
+            TCP.conv_process()
+        elif input == "M":
+            MessageQueues.conv_process()
+        elif input == "P":
+            PipesKelvin.conv_process()
+        os._exit(0)
 
     if input == "T":
-        ProzessIDs = []
-    
-        ProzessIDs.append(os.fork())
-        if ProzessIDs[-1] == 0:
-            TCP.conv_process()
-            os._exit(0)
-
         time.sleep(5)
 
-        ProzessIDs.append(os.fork())
-        if ProzessIDs[-1] == 0:
+    ProzessIDs.append(os.fork())
+    if ProzessIDs[-1] == 0:
+        if input == "T":
             TCP.log_process()
-            os._exit(0)
-    
-        ProzessIDs.append(os.fork())
-        if ProzessIDs[-1] == 0:
-            TCP.stat_process()
-            os._exit(0)
-
-        time.sleep(5)
-    
-        ProzessIDs.append(os.fork())
-        if ProzessIDs[-1] == 0:
-            TCP.report_process()
-            os._exit(0)
-
-        signal.signal(signal.SIGINT, signal_handler_TCP)
-    
-        while True:
-            try:
-                time.sleep(1)
-            except KeyboardInterrupt:
-                signal_handler_TCP(signal.SIGINT, None)
-
-#------------------------------------------------------------------------------------------------------               
-
-    elif input == "M":
-        MessageQueues.CreateQueue()
-
-        ProzessIDs = []
-    
-        ProzessIDs.append(os.fork())
-        if ProzessIDs[-1] == 0:
-            MessageQueues.conv_process()
-            os._exit(0)
-
-        ProzessIDs.append(os.fork())
-        if ProzessIDs[-1] == 0:
-            MessageQueues.log_process()
-            os._exit(0)
-    
-        ProzessIDs.append(os.fork())
-        if ProzessIDs[-1] == 0:
-            MessageQueues.stat_process()
-            os._exit(0)
-    
-        ProzessIDs.append(os.fork())
-        if ProzessIDs[-1] == 0:
-            MessageQueues.report_process()
-            os._exit(0)
-
-        signal.signal(signal.SIGINT, signal_handler_TCP)
-    
-        while True:
-            try:
-                time.sleep(1)
-            except KeyboardInterrupt:
-                signal_handler_MQ(signal.SIGINT, None)
-
-#------------------------------------------------------------------------------------------------------ 
-    elif input == "P":
-
-        PipesKelvin.benannte_pipes()
-        ProzessIDs = []
-    
-        ProzessIDs.append(os.fork())
-        if ProzessIDs[-1] == 0:
-            PipesKelvin.conv_process()
-            os._exit(0)
-
-        ProzessIDs.append(os.fork())
-        if ProzessIDs[-1] == 0:
+        elif input == "M":
+            MessageQueues.log_process() 
+        elif input == "P":
             PipesKelvin.log_process()
-            os._exit(0)
+        os._exit(0)
     
-        ProzessIDs.append(os.fork())
-        if ProzessIDs[-1] == 0:
-            PipesKelvin.stat_process()
-            os._exit(0)
-    
-        ProzessIDs.append(os.fork())
-        if ProzessIDs[-1] == 0:
-            PipesKelvin.report_process()
-            os._exit(0)
+    ProzessIDs.append(os.fork())
+    if ProzessIDs[-1] == 0:
+        if input == "T":
+            TCP.stat_process()
+        elif input == "M":
+            MessageQueues.stat_process()    
+        elif input == "P":
+            PipesKelvin.stat_process()        
+        os._exit(0)
 
-        signal.signal(signal.SIGINT, signal_handler_Pipes)
+    if input == "T":
+        time.sleep(5)
+
+    ProzessIDs.append(os.fork())
+    if ProzessIDs[-1] == 0:
+        if input == "T":
+            TCP.report_process()
+        elif input == "M":
+            MessageQueues.report_process() 
+        elif input == "P":
+            PipesKelvin.report_process()
+        os._exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler_TCP)
     
-        while True:
-            try:
-                time.sleep(1)
-            except KeyboardInterrupt:
+    while True:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            if input == "T":
+                signal_handler_TCP(signal.SIGINT, None)
+            elif input == "M":
+                signal_handler_MQ(signal.SIGINT, None)
+            elif input == "P":
                 signal_handler_Pipes(signal.SIGINT, None)
