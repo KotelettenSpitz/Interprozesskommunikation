6cfad23ada148e53cb3b31646a1c24176e633625
OrhanYu <orhan.akcora@outlook.de>
2024-06-29 13:29:29 +0200

    Tabelle bei Ausgabe nun in die Main getan (kürzerer Gesamtcode)

--- Main.py
+++ Main.py
@@ -31,32 +31,47 @@ if __name__ == "__main__":
 
     input = input("Wählen sie ihre Implementierungsvariante: ")
 
-#------------------------------------------------------------------------------------------------------  
+    if input == "T" or input == "S" or input == "M" or input == "P":
+        # Tabellenüberschrift und Design für die Ausgabe
+        spalten = ["Zeit", "Summe", "Mittelwert"]
+        design = "+" + "+".join(["-" * (len(col) + 2) for col in spalten]) + "+"
+
+        # Tabellenüberschrift ausgeben
+        print(design)
+        print("|", end="")
+        for col in spalten:
+            print(f" {col} |", end="")
+        print("\n" + design)
+ 
     ProzessIDs = []
-    
+
     if input == "T":
         ProzessIDs.append(prozess_starten(TCP.conv_process))
         ProzessIDs.append(prozess_starten(TCP.log_process))
         ProzessIDs.append(prozess_starten(TCP.stat_process))
         ProzessIDs.append(prozess_starten(TCP.report_process))
 
-    if input == "M":
+    elif input == "M":
         ProzessIDs.append(prozess_starten(MessageQueues.conv_process))
         ProzessIDs.append(prozess_starten(MessageQueues.log_process))
         ProzessIDs.append(prozess_starten(MessageQueues.stat_process))
         ProzessIDs.append(prozess_starten(MessageQueues.report_process))
 
-    if input == "P":
+    elif input == "P":
         ProzessIDs.append(prozess_starten(PipesKelvin.conv_process))
         ProzessIDs.append(prozess_starten(PipesKelvin.log_process))
         ProzessIDs.append(prozess_starten(PipesKelvin.stat_process))
         ProzessIDs.append(prozess_starten(PipesKelvin.report_process))
 
-    if input == "S":
+    elif input == "S":
         ProzessIDs.append(prozess_starten(Conv.conv_process))
         ProzessIDs.append(prozess_starten(Log.log_process))
         ProzessIDs.append(prozess_starten(Stat.stat_process))
         ProzessIDs.append(prozess_starten(Report.report_process))
+    
+    else:
+        print("Eingabe nicht erkannt")
+        exit()
 
     signal.signal(signal.SIGINT, signal_handler)
     
